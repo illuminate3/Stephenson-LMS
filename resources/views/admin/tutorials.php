11147400dbd1cd48d4592495325b7edb0f9f7951
LuanToninGalvan <ltoningalvan@gmail.com>
2018-01-06 16:40:05 -0200

    Views de Tutorial

---
+++ resources/views/admin/tutorials.php
@@ -0,0 +1,36 @@
+<div class="container">
+	<h2>Tutoriais</h2>
+	
+	<?php			
+		if(count($tutorials) < 1){
+			echo "<p>Nenhum tutorial cadastrado.</p>";
+		} else{
+	?>
+	<table class="highlight responsive-table">
+		<thead>
+			<tr>
+				<td>Titulo</td>
+				<td>Resumo</td>
+				<td>Url</td>
+				<td>Autor</td>
+				<td>Criação</td>
+				<td></td>
+			</tr>
+		</thead>
+		
+		<tbody>
+			<?php foreach($tutorials as $tutorial) { ?>
+			<tr>
+				<td><a href="<?php echo URL::to('/admin/tutorials/edit/'. $tutorial['id'] ); ?>"><?php echo $tutorial['title']; ?></a></td>
+				<td><?php echo $tutorial['resume']; ?></td>
+				<td><?php echo $tutorial['video_url']; ?></td>
+				<td><?php echo $tutorial['author']; ?></td>
+				<td><?php echo $tutorial['created_at']; ?></td>
+				<td>
+					<a href="#"><i class="material-icons">remove_circle_outline</i></a>
+				</td>
+			</tr>
+			<?php }}?>
+		</tbody>
+	</table>
+</div>
---
+++ resources/views/tutorials/tutorials.php
@@ -0,0 +1,9 @@
+<main>
+	<div class="container">
+		<div class="section">
+			<div id="page-title">
+				<h2>Tutoriais</h2>
+			</div>
+		</div>
+	</div>
+</main>
