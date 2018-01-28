3f70a25bf0a9f05110a37745217cc4620121cf76
LuanToninGalvan <ltoningalvan@gmail.com>
2018-01-28 19:46:00 -0200

    Documentação

---
+++ docs/script.js
@@ -0,0 +1,9 @@
+	$('.dropdown-menu-item').click(function(){
+		if($(this).find('ul').is(':visible')){
+			$(this).find('ul').slideUp();
+		} else{
+			$('.dropdown-menu-item ul').slideUp();
+			$(this).find('ul').slideDown();
+		}
+	});
+	
